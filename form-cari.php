b7c18e24c5777c0ab5597fb770b4a73316e893f3
dwi sakethi <dwijim@gmail.com>
2021-07-06 19:09:45 +0700

    Create form-cari.php

---
+++ form-cari.php
@@ -0,0 +1,11 @@
+<?php
+/* ------------------------------------------------
+   form untuk contoh melakukan pencarian
+   berkas ini diberi nama: form-cari.php 
+   dibuat pada tanggal:
+   - 6 juli 2021
+
+   ------------------------------------------------ */
+
+
+?>
